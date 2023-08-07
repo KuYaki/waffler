cbb3950c6c77ea3b6bc0e942b1a2f17109dffffa
jakov.kobozev <jakov.kobozev@yandex.ru>
2023-08-07 11:23:29 +0300

    added single choice dropdown

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
     import { ref } from 'vue';
 
-    import InputText      from '@/components/input/InputText.vue'
-    import SourceDropdown from '@/components/dropdown/SourceDropdown.vue';
-    import ToggleTheme    from '@/components/input/ToggleTheme.vue';
-    import SignInTab      from '@/views/tabs/SignInTab.vue'
-    import Divider        from 'primevue/divider';
+    import InputText            from '@/components/input/InputText.vue'
+    import SourceDropdown       from '@/components/dropdown/SourceDropdown.vue';
+    import SingleChoiceDropdown from '@/components/dropdown/SingleChoiceDropdown.vue'
+    import ToggleTheme          from '@/components/input/ToggleTheme.vue';
+    import SignInTab            from '@/views/tabs/SignInTab.vue'
+    import Divider              from 'primevue/divider';
 
     const searchValue = ref('some links')
 </script>
@@ -16,8 +17,11 @@
         <InputText v-model:value = 'searchValue'/>
         {{ searchValue }}
 
-        <Divider align="center">Multiselect</Divider>
+        <Divider align="center">Multiselect Dropdown</Divider>
         <SourceDropdown/>
+
+        <Divider align="center">SingleChoiceDropdown Dropdown</Divider>
+        <SingleChoiceDropdown/>
 
         <Divider align="center"> Theme toggle </Divider>
         <ToggleTheme/>
