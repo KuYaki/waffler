e2a0f17fa427923985dbc5d09feb51df12232b82
jakov.kobozev <jakov.kobozev@yandex.ru>
2023-08-17 16:05:59 +0300

    created profile dlg

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -6,12 +6,15 @@
     import MainPageTable from '@/components/table/MainPageTable.vue';
     import AddTokenDlg   from '@/views/dialog/AddTokenDlg.vue';
     import SignInDlg     from '@/views/dialog/SignInDlg.vue'
+    import ProfileDlg     from '@/views/dialog/ProfileDlg.vue';
+
     import InputText     from '@/components/input/InputText.vue';
     import Button        from 'primevue/button';
     import Dialog        from 'primevue/dialog';
 
     const isAddTokenDlgOpen = ref(false)
     const isSignInDlgOpen   = ref(false)
+    const isProfileDlgOpen  = ref(true)
 
 
     const openAddNewTokendDlg = () =>{
@@ -22,6 +25,10 @@
         isSignInDlgOpen.value = true
     }
 
+    const openProfileDlg = () => {
+        isProfileDlgOpen.value = true
+    }
+
 </script>
 
 <template>
@@ -30,7 +37,7 @@
     />
 
     <div class="main_page">
-        <InputText/>
+        <InputText />
         <SearchParam/>
         <MainPageTable>
             <Button
@@ -50,6 +57,19 @@
                 maxWidth:'500px' }">
 
             <AddTokenDlg/>
+
+       </Dialog>
+
+       <Dialog
+            v-model:visible="isProfileDlgOpen"
+            :modal="true"
+            :dismissableMask="true"
+            header=" "
+            :style="{
+                width: '90vw',
+                maxWidth:'500px' }">
+
+            <ProfileDlg/>
 
        </Dialog>
 
---
+++ src/views/dialog/ProfileDlg.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    import Profile   from '@/components/avatar/Profile.vue'
+    import Dropdown  from 'primevue/dropdown';
+    import Button    from 'primevue/button';
+    import ProfileTable from '@/components/table/ProfileTable.vue';
+
+    const currentField = ref( { name: 'Waffler',  code: 'W' });
+    const fields = ref([
+        { name: 'Waffler',  code: 'W' },
+        { name: 'Racizm',   code: 'R' },
+    ]);
+
+    const loading = ref(false)
+
+    const load = () =>{
+        loading.value = true
+        setTimeout(()=> loading.value = false , 2000)
+    }
+
+</script>
+
+<template>
+    <div class="profile_block">
+        <div class="head">
+
+            <Profile />
+
+            <Dropdown
+                v-model="currentField"
+                display="chip"
+                :options="fields"
+                optionLabel="name"/>
+
+        </div>
+
+        <ProfileTable />
+
+        <Button
+            label="Primary"
+            :loading="loading"
+            @click="load"
+            outlined />
+    </div>
+</template>
+
+
+<style scoped>
+    .profile_block {
+        display: grid;
+        grid-template-columns: 100%;
+        row-gap: 10px;
+    }
+
+    .head {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        align-items: center;
+    }
+
+</style>
