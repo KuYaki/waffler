d5580be521cdc5a1932673aeb22010ee34bf484b
jakov.kobozev <jakov.kobozev@yandex.ru>
2023-09-07 20:34:08 +0300

    Added  api routes

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,16 @@ class API extends String {}
 
 class APIRoute {
 
-    static LOGIN:API = '/login'
-    static LOGOUT:API = '/logout'
+    static SOURCE_SEARCH:API = '/source/search'
+    static SOURCE_SCORE:API  = '/source/score'
+    static SOURCE_INFO:API   = '/source/info'
+
+    static LOGIN:API        = '/user/auth'
+    static REGISTRATION:API = '/user/register'
+    static LOGOUT:API       = '/logout'
+
+    static USER_INFO:API = '/user/info'
+    static USER_SAVE:API = '/user/save'
 }
 
 export default APIRoute
